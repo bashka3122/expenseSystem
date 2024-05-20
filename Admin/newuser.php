bb1d3c2dfbacfd5e4bf79dc1f489dc26a4edb003
bashka3122 <bcsalan53@gmail.com>
2024-05-20 17:08:51 +0300

    new users title updated

--- Admin/newuser.php
+++ Admin/newuser.php
@@ -10,7 +10,7 @@ if (!$_SESSION['userID']) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dashboard</title>
+    <title>Expense management System-New users</title>
 
 </head>
 
